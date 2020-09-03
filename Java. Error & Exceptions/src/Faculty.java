deed3d91631b2414f01cc3e8529907cb734ecf35
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-03 08:41:21 +0600

    Faculty class in progress

--- Java. Error & Exceptions/src/Faculty.java
+++ Java. Error & Exceptions/src/Faculty.java
@@ -1,2 +1,30 @@
+import java.util.List;
+
 public class Faculty {
+    private String name;
+    private List<StudentGroup> studentGroupList;
+
+    public Faculty {
+    }
+
+    public Faculty(String name) {
+        this.name = name;
+    }
+
+    public Faculty(String name, List<StudentGroup> studentGroupList) {
+        this.name = name;
+        setStudentGroups();
+    }
+
+    public void getFacultyName() {
+    }
+
+    public void setFacultyName() {
+    }
+
+    public void getStudentGroups() {
+    }
+
+    public void setStudentGroups() {
+    }
 }
--- Java. Error & Exceptions/src/StudentGroup.java
+++ Java. Error & Exceptions/src/StudentGroup.java
@@ -1,4 +1,17 @@
+import java.util.List;
+
 public class StudentGroup {
-//Факультет
+    private String name;
+    private List<Student> listOfStudents;
+
+    public StudentGroup(String name) {
+        this.name = name;
+    }
+
+    public StudentGroup(String name, List<Student> listOfStudents) {
+        this.name = name;
+
+    }
+
 
 }
