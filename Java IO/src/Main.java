31f18ae26e403da5f743c376445627dbeefab3e7
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-16 11:31:01 +0600

    work in progress

--- Java IO/src/Main.java
+++ Java IO/src/Main.java
@@ -1,17 +1,29 @@
 import java.io.File;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter path");
-        String string = scanner.nextLine();
-        System.out.println(string);
-        File directory = new File(string);
-        if (directory.exists() && directory.isDirectory()) {
-            for (File current : directory.listFiles()) {
-                System.out.println(current.getPath());
+    public static void main(String args[]) throws IOException {
+        File directoryPath = new File("C:\\Users\\PC\\Desktop\\Misc");
+        File[] contents = directoryPath.listFiles();
+        if (directoryPath.isDirectory()) {
+        } else if (directoryPath.isFile()) {
+            System.out.println("Это файл");
+        }
+        System.out.println("├───");
+        
+
+
+
+
+        for (int i = 0; i < contents.length; i++) {
+            if (contents[i].isFile()) {
+                System.out.println("Is file");
+            } else if (contents[i].isDirectory()) {
+                System.out.println("Is Directory");
             }
         }
+    }
+
+    public static void listTree() {
     }
 }
