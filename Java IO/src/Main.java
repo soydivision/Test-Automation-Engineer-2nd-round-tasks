90d03777b5ca5704cccf91029703acbfe4697ec8
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-16 14:31:38 +0600

    list files in tree style method added, tested

--- Java IO/src/Main.java
+++ Java IO/src/Main.java
@@ -3,27 +3,47 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        File directoryPath = new File("C:\\Users\\PC\\Desktop\\Misc");
+        File directoryPath = new File("C:\\Users\\PC\\Desktop\\Music");
         File[] contents = directoryPath.listFiles();
         if (directoryPath.isDirectory()) {
-        } else if (directoryPath.isFile()) {
-            System.out.println("Это файл");
-        }
-        System.out.println("├───");
-        
-
-
-
-
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[i].isFile()) {
-                System.out.println("Is file");
-            } else if (contents[i].isDirectory()) {
-                System.out.println("Is Directory");
+            StringBuilder stringBuilderFiles = new StringBuilder();
+            StringBuilder stringBuilderFolders = new StringBuilder();
+            stringBuilderFolders.append("├───" + directoryPath.getName() + "\n");
+            for (int i = 0; i < contents.length; i++) {
+                if (contents[i].isDirectory()) {
+                    stringBuilderFolders.append("│   └───");
+                    stringBuilderFolders.append(contents[i].getName() + "\n");
+                    // и какой-нибудь листТриФолдер
+                } else if (contents[i].isFile()) {
+                    //Добавить файлы в строку какую-то.
+                    stringBuilderFiles.append("│   " + contents[i].getName() + "\n");
+                }
             }
+            stringBuilderFolders.append(stringBuilderFiles);
+            System.out.println(stringBuilderFolders);
+        } else if (directoryPath.isFile()) {
+            System.out.println("Это файл ");
+//Если в качестве параметра в программу передается не путь к директории,
+// а путь к txt файлу по образцу выше - прочитать файл и вывести в консоль следующие данные:
+//    • Количество папок
+//    • Количество файлов
+//    • Среднее количество файлов в папке
+//    • Среднюю длинну названия файла
         }
+        System.out.println("TEST");
+        StringBuilder test = listFilesTree(directoryPath);
+        System.out.println(test);
     }
 
-    public static void listTree() {
+    public static StringBuilder listFilesTree(File directoryWithFiles) {
+        StringBuilder stringBuilderTreeFiles = new StringBuilder();
+        File[] content = directoryWithFiles.listFiles();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i].isFile()) {
+                //Добавить файлы в строку какую-то.
+                stringBuilderTreeFiles.append("│   " + content[i].getName() + "\n");
+            }
+        }
+        return stringBuilderTreeFiles;
     }
 }
