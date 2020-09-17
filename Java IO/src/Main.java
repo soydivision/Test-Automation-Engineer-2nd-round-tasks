ee8b4a8e4821b9ec8884501da570a7bf83cfcf99
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-17 14:29:41 +0600

    many dramatic changes, comment won't do justice

--- Java IO/src/Main.java
+++ Java IO/src/Main.java
@@ -1,49 +1,47 @@
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String args[]) throws IOException {
-        File directoryPath = new File("C:\\Users\\PC\\Desktop\\Music");
-        File[] contents = directoryPath.listFiles();
-        if (directoryPath.isDirectory()) {
-            StringBuilder stringBuilderFiles = new StringBuilder();
-            StringBuilder stringBuilderFolders = new StringBuilder();
-            stringBuilderFolders.append("├───" + directoryPath.getName() + "\n");
-            for (int i = 0; i < contents.length; i++) {
-                if (contents[i].isDirectory()) {
-                    stringBuilderFolders.append("│   └───");
-                    stringBuilderFolders.append(contents[i].getName() + "\n");
-                    // и какой-нибудь листТриФолдер
-                } else if (contents[i].isFile()) {
-                    //Добавить файлы в строку какую-то.
-                    stringBuilderFiles.append("│   " + contents[i].getName() + "\n");
-                }
-            }
-            stringBuilderFolders.append(stringBuilderFiles);
-            System.out.println(stringBuilderFolders);
-        } else if (directoryPath.isFile()) {
-            System.out.println("Это файл ");
-//Если в качестве параметра в программу передается не путь к директории,
-// а путь к txt файлу по образцу выше - прочитать файл и вывести в консоль следующие данные:
-//    • Количество папок
-//    • Количество файлов
-//    • Среднее количество файлов в папке
-//    • Среднюю длинну названия файла
-        }
-        System.out.println("TEST");
-        StringBuilder test = listFilesTree(directoryPath);
-        System.out.println(test);
+        File file = new File("C:\\Users\\PC\\Desktop\\TestFolder");
+        System.out.println(file.getName());
+        System.out.println(listFilesTree(file) + "\n");
+        System.out.println();
     }
 
-    public static StringBuilder listFilesTree(File directoryWithFiles) {
+    public static StringBuilder listFilesTree(File directory) {
         StringBuilder stringBuilderTreeFiles = new StringBuilder();
-        File[] content = directoryWithFiles.listFiles();
+        File[] content = directory.listFiles();
         for (int i = 0; i < content.length; i++) {
             if (content[i].isFile()) {
-                //Добавить файлы в строку какую-то.
-                stringBuilderTreeFiles.append("│   " + content[i].getName() + "\n");
+                stringBuilderTreeFiles.append("│" + generateFourSpaces(howDeepIsYourPath(directory, content[i])) + content[i].getName() + "\n");
+            } else if (content[i].isDirectory()) {
+                stringBuilderTreeFiles.append(listFilesTree(content[i]));
+                System.out.print("└───");
+                System.out.println(content[i].getName());
+                System.out.println(listFilesTree(content[i]));
             }
         }
         return stringBuilderTreeFiles;
+    }
+
+    public static int howDeepIsYourPath(File parentDirectory, File childDirectory) {
+        int depth;
+        Path parentPath = Paths.get(parentDirectory.toURI());
+        Path childPath = Paths.get(childDirectory.toURI());
+        depth = childPath.getNameCount() - parentPath.getNameCount();
+        return depth;
+    }
+
+    public static String generateFourSpaces(int length) {
+        String shift = "   ";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append(shift);
+        }
+        return stringBuilder.toString();
     }
 }
