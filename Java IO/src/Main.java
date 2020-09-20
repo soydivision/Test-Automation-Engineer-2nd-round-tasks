b8a4a3e36b49b68c30c878d731e57927b95d87e9
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-09-20 13:09:32 +0600

    many minor methods added, works, not perfect

--- Java IO/src/Main.java
+++ Java IO/src/Main.java
@@ -1,74 +1,53 @@
 import java.io.File;
+import java.lang.reflect.Array;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
-        File file = new File("C:\\Users\\PC\\Desktop\\Music");
-        System.out.println(file.getName());
-        File[] contents = directory.listFiles();
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[i].isFile()) {
-                System.out.println("    " + contents[i].getName());
-            } else if (contents[i].isDirectory()) {
-                System.out.println("----" + contents[i].getName());
-                listFilesTree(contents[i]);
+        File file = new File("C:\\Users\\PC\\Desktop\\Torrent");
+        listFilesTree(file, file);
+
+    }
+
+    public static void listFilesTree(File directoryWithFiles, File parent) {
+        String space = generateFourSpaces(howDeepIsYourPath(directoryWithFiles, parent));
+        if (howDeepIsYourPath(directoryWithFiles, parent) == 0) {
+            System.out.println(directoryWithFiles.getName());
+        } else if (directoryWithFiles.isDirectory()) {
+            System.out.println(space + "└───" + directoryWithFiles.getName());
+        }
+        File[] content = directoryWithFiles.listFiles();
+        ArrayList<File> folders = new ArrayList<>();
+        for (int i = 0; i < content.length; i++) {
+            if (content[i].isFile()) {
+                String spaceForFiles = generateFourSpaces(howDeepIsYourPath(content[i], parent));
+                System.out.println(spaceForFiles + "    " + content[i].getName());
+            } else if (content[i].isDirectory()) {
+                folders.add(content[i]);
             }
+        }
+        for (int i = 0; i < folders.size(); i++) {
+            listFilesTree(folders.get(i), parent);
         }
     }
 
-    public static void listFilesTree(File directory) {
-        //Полный листинг файлов (для начала)
-        //Попутно проверка наличия папок
-        //При нахождении папок они отрисовывают (отдельный метод?)
-        // в конце списка линии в parent директорию и вызывают листинг файлов
-        //на себя
-        File[] contents = directory.listFiles();
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[i].isFile()) {
-                System.out.println("    " + contents[i].getName());
-                //Добавить в стринг билдер
-            } else if (contents[i].isDirectory()) {
-//               Вызов метода который снова будет листить эту папку. И другой дорисует стрелки.
-                System.out.println("----" + contents[i].getName());
-                listFilesTree(contents[i]);
-            }
-        }
+    public static int howDeepIsYourPath(File parentDirectory, File childDirectory) {
+        int depth;
+        Path parentPath = Paths.get(parentDirectory.toURI());
+        Path childPath = Paths.get(childDirectory.toURI());
+        depth = childPath.getNameCount() - parentPath.getNameCount();
+        return Math.abs(depth);
     }
 
-    public static void listFilesAndFoldersTree(File directory) {
-        //Полный листинг файлов (для начала)
-        //Попутно проверка наличия папок
-        //При нахождении папок они отрисовывают (отдельный метод?)
-        // в конце списка линии в parent директорию и вызывают листинг файлов
-        //на себя
-        File[] contents = directory.listFiles();
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[i].isFile()) {
-                System.out.println("    " + contents[i].getName());
-                //Добавить в стринг билдер
-            } else if (contents[i].isDirectory()) {
-//               Вызов метода который снова будет листить эту папку. И другой дорисует стрелки.
-                System.out.println("----" + contents[i].getName());
-                listFilesTree(contents[i]);
-            }
+    public static String generateFourSpaces(int length) {
+        String shift = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            stringBuilder.append("    ");
         }
-    }
-
-    public static void listFoldersTree(File directory) {
-        //Полный листинг файлов (для начала)
-        //Попутно проверка наличия папок
-        //При нахождении папок они отрисовывают (отдельный метод?)
-        // в конце списка линии в parent директорию и вызывают листинг файлов
-        //на себя
-        File[] contents = directory.listFiles();
-        for (int i = 0; i < contents.length; i++) {
-            if (contents[i].isFile()) {
-                System.out.println("    " + contents[i].getName());
-                //Добавить в стринг билдер
-            } else if (contents[i].isDirectory()) {
-//               Вызов метода который снова будет листить эту папку. И другой дорисует стрелки.
-                System.out.println("----" + contents[i].getName());
-                listFilesTree(contents[i]);
-            }
-        }
+        return stringBuilder.toString();
     }
 }
+
