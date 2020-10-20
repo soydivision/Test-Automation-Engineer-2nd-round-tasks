dadf060d0abcfcc5a84dbe65826e87cded0f5420
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-10-20 07:55:56 +0600

    minor changes, working

--- my-app/src/main/java/com/mycompany/threads/Dock.java
+++ my-app/src/main/java/com/mycompany/threads/Dock.java
@@ -33,13 +33,13 @@ public class Dock implements Runnable {
                             containersToUnload -= unloadSpeed;
                             System.out.println("'" + Thread.currentThread().getName() + "'" + " is reporting, unloading ship: " + ship.getId());
                             System.out.println("'" + Thread.currentThread().getName() + "':" + "Currently,containers left on ship: " + containersToUnload);
-                            System.out.println();
                             System.out.println("Containers stored in port, total:" + port.containers);
+                            System.out.println();
                         } else if (containersToUnload == 0) {
                             System.out.println("Ship " + ship.getId() + " is fully unloaded");
                         } else if (containersToUnload != 0 && containersToUnload - unloadSpeed < 0) {
                             System.out.println("Small container number remains on ship: " + ship.getId() + " : " + ship.getContainersOnShip());
-                            System.out.println("Container to unload " + containersToUnload);
+                            System.out.println("Containers to unload " + containersToUnload);
                             System.out.println("'" + Thread.currentThread().getName() + "'" + " is reporting, unloading ship: " + ship.getId());
                             port.addToStorage(ship.getContainersOnShip());
                             containersToUnload = containersToUnload - ship.getContainersOnShip();
