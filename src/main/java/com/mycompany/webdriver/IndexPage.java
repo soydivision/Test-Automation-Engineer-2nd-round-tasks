5995bc1b5656eca28b948db1e881e29910c034b1
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2020-11-05 12:39:11 +0600

    Page object created, pastebin name and text works

---
+++ src/main/java/com/mycompany/webdriver/IndexPage.java
@@ -0,0 +1,56 @@
+package com.mycompany.webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class IndexPage {
+    //    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+//        driver.get("https://pastebin.com/");
+//    }
+    private WebDriver driver;
+
+    public void indexPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void openIndexPage() {
+        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+        driver.get("https://pastebin.com/");
+    }
+
+    public void pasteBin(String pasteBin) {
+        driver.findElement(By.xpath("//*[@id=\"postform-text\"]")).sendKeys(pasteBin);
+    }
+
+    public void pasteBinName(String name) {
+        driver.findElement(By.xpath("//*[@id=\"postform-name\"]")).sendKeys(name);
+    }
+//    public void pasteBinName(String name) {
+//        driver.findElement(By.xpath("//*[@id=\"postform-name\"]")).sendKeys(name);
+//    }
+
+    public void setExpiration(String expiration) {
+        WebElement element = driver.findElement(By.id("postform-expiration"));
+        Select select = new Select(element);
+        List<WebElement> list = select.getOptions();
+        for (WebElement webElement : list) {
+            String innerText = webElement.getAttribute("innerText");
+            System.out.println(innerText);
+            if (innerText.equals("expiration")) {
+                element.click();
+            }
+        }
+    }
+
+    public void setSyntaxHiglighting(String pasteBin) {
+    }
+}
--- src/main/java/com/mycompany/webdriver/Main.java
+++ src/main/java/com/mycompany/webdriver/Main.java
@@ -12,23 +12,30 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+//        driver.get("https://pastebin.com/");
+//        driver.manage().window().maximize();
+//        driver.findElement(By.xpath("//*[@id=\"postform-text\"]")).sendKeys("Hello from WD");
+//        driver.findElement(By.xpath("//*[@id=\"postform-name\"]")).sendKeys("helloweb");
+//        WebElement element = driver.findElement(By.id("postform-expiration"));
+//        Select select = new Select(element);
+//        List<WebElement> list = select.getOptions();
+//        for (WebElement webElement : list) {
+//            String innerText = webElement.getAttribute("innerText");
+//            System.out.println(innerText);
+//            if (innerText.equals("10 Minutes")) {
+//                element.click();
+//            }
+//        }
+//        driver.quit();
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\PC\\Desktop\\Selenium\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-        driver.get("https://pastebin.com/");
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//*[@id=\"postform-text\"]")).sendKeys("Hello from WD");
-        driver.findElement(By.xpath("//*[@id=\"postform-name\"]")).sendKeys("helloweb");
-        WebElement element = driver.findElement(By.id("postform-expiration"));
-        Select select = new Select(element);
-        List<WebElement> list = select.getOptions();
-        for (WebElement webElement : list) {
-            String innerText = webElement.getAttribute("innerText");
-            System.out.println(innerText);
-            if (innerText.equals("10 Minutes")) {
-                element.click();
-            }
-        }
-
+        IndexPage page = new IndexPage();
+        page.indexPage(driver);
+        page.openIndexPage();
+        page.pasteBin("dsjfklsdjf");
+        page.pasteBinName("benny   hill");
     }
 }
